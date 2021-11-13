0a265d93b2c5a69ab98f9fd1a5cbac00151b2470
Amminn <vrb.nnn@gmail.com>
2021-11-13 10:46:07 +0100

    fixing sound play

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let sounds = document.querySelectorAll('audio')
 for (let i = 0; i < sounds.length; i++) {
     document.addEventListener("keydown", function(event) {
         if (event.keyCode == sounds[i].getAttribute('data-key')) {
+            sounds[i].currentTime = 0;
             keys[i].classList.add('playing');
             sounds[i].play()
             setTimeout(function() {
